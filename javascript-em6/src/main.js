fe44878b5e2201ab46fb0264361ac0c3a10d283b
Jrdutra <jrdutra.com.br@gmail.com>
2019-07-01 19:49:10 -0300

    Estudo do Assync e Await

--- javascript-em6/src/main.js
+++ javascript-em6/src/main.js
@@ -1,1 +1,14 @@
-alert('');
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username){
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response.data);
+        } catch (err){
+            console.warn('Erro na API: ');
+        }
+    }
+}
+
+Api.getUserInfo('jrdutra2323553');
